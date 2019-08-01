a552d894e3b56111d1d88a0e7308b9eae4914407
kinga bernhardt <kinga.bernhardt@rpharms.com>
2019-08-01 14:19:39 +0100

    Correct failing tests for ISP

--- SolidSample/ArdalisRating.Tests/JsonPolicySerializerGetPolicyFromJsonString.cs
+++ SolidSample/ArdalisRating.Tests/JsonPolicySerializerGetPolicyFromJsonString.cs
@@ -19,41 +19,41 @@ namespace ArdalisRating.Tests
                 AssertPoliciesEqual(result, policy);
             }
 
-            [Fact]
-            public void ReturnsSimpleAutoPolicyFromValidJsonString()
+        [Fact]
+        public void ReturnsSimpleAutoPolicyFromValidJsonString()
+        {
+            var inputJson = @"{
+  ""type"": ""Auto"",
+  ""make"": ""BMW""
+}
+";
+            var serializer = new JsonPolicySerializer();
+
+            var result = serializer.GetPolicyFromJsonString(inputJson);
+
+            var policy = new Policy
             {
-                var inputJson = @"{
-                        ""type"": ""Auto"",
-                        ""make"": ""BMW""
-                        }
-                            ";
-                var serializer = new JsonPolicySerializer();
+                Type = "Auto",
+                Make = "BMW"
+            };
+            AssertPoliciesEqual(result, policy);
+        }
 
-                var result = serializer.GetPolicyFromJsonString(inputJson);
-
-                var policy = new Policy
-                {
-                    //Type = PolicyType.Auto,
-                    Make = "BMW"
-                };
-                AssertPoliciesEqual(result, policy);
-            }
-
-            private static void AssertPoliciesEqual(Policy result, Policy policy)
-            {
-                Assert.Equal(policy.Address, result.Address);
-                Assert.Equal(policy.Amount, result.Amount);
-                Assert.Equal(policy.BondAmount, result.BondAmount);
-                Assert.Equal(policy.DateOfBirth, result.DateOfBirth);
-                Assert.Equal(policy.Deductible, result.Deductible);
-                Assert.Equal(policy.FullName, result.FullName);
-                Assert.Equal(policy.IsSmoker, result.IsSmoker);
-                Assert.Equal(policy.Make, result.Make);
-                Assert.Equal(policy.Miles, result.Miles);
-                Assert.Equal(policy.Model, result.Model);
-                Assert.Equal(policy.Type, result.Type);
-                Assert.Equal(policy.Valuation, result.Valuation);
-                Assert.Equal(policy.Year, result.Year);
-            }
+        private static void AssertPoliciesEqual(Policy result, Policy policy)
+        {
+            Assert.Equal(policy.Address, result.Address);
+            Assert.Equal(policy.Amount, result.Amount);
+            Assert.Equal(policy.BondAmount, result.BondAmount);
+            Assert.Equal(policy.DateOfBirth, result.DateOfBirth);
+            Assert.Equal(policy.Deductible, result.Deductible);
+            Assert.Equal(policy.FullName, result.FullName);
+            Assert.Equal(policy.IsSmoker, result.IsSmoker);
+            Assert.Equal(policy.Make, result.Make);
+            Assert.Equal(policy.Miles, result.Miles);
+            Assert.Equal(policy.Model, result.Model);
+            Assert.Equal(policy.Type, result.Type);
+            Assert.Equal(policy.Valuation, result.Valuation);
+            Assert.Equal(policy.Year, result.Year);
+        }
     }
 }
--- SolidSample/ArdalisRating.Tests/RatingEngineRate.cs
+++ SolidSample/ArdalisRating.Tests/RatingEngineRate.cs
@@ -12,7 +12,7 @@ namespace ArdalisRating.Tests
         {
             var policy = new Policy
             {
-                //Type = PolicyType.Land,
+                Type = "Land",
                 BondAmount = 200000,
                 Valuation = 200000
             };
