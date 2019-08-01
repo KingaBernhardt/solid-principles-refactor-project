bb048d8adeabde0056fdbb5d3f40b9aae3179f92
kinga bernhardt <kinga.bernhardt@rpharms.com>
2019-08-01 11:29:58 +0100

    Add project (not the initial)

---
+++ SolidSample/ArdalisRating/RatingEngine.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.IO;
+
+namespace ArdalisRating
+{
+    /// <summary>
+    /// The RatingEngine reads the policy application details from a file and produces a numeric 
+    /// rating value based on the details.
+    /// </summary>
+    public class RatingEngine
+    {
+        public ConsoleLogger Logger { get; set; } = new ConsoleLogger();
+        public FilePolicySource PolicySource { get; set; }
+        public JsonPolicySerializer PolicySerializer { get; set; }
+        public decimal Rating { get; set; }
+        public void Rate()
+        {
+            Logger.Log("Starting rate.");
+
+            Logger.Log("Loading policy.");
+
+            // load policy - open file policy.json
+            string policyJson = PolicySource.GetPolicyFromSource();
+
+            var policy = PolicySerializer.GetPolicyFromJsonString(policyJson)
+;
+            var factory = new RaterFactory();
+            var rater = factory.Create(policy, this);
+            rater?.Rate(policy);
+
+            Logger.Log("Rating completed.");
+        }
+    }
+}
